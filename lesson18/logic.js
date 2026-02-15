14c4d1607a297178a6daab526e51463d8cfab256
Abdisa Awel <abdisaawel313@gmail.com>
2026-02-15 00:20:07 -0800

    Lesson 18: Fix bug in discount logic

--- lesson18/logic.js
+++ lesson18/logic.js
@@ -2,7 +2,7 @@ function calculateDiscount(price, discountPercentage) {
     if (price < 0) throw new Error("Price cannot be negative");
     if (discountPercentage < 0 || discountPercentage > 100) throw new Error("Invalid discount");
     
-    return price + (price * (discountPercentage / 100));
+    return price - (price * (discountPercentage / 100));
 }
 
 module.exports = { calculateDiscount };
